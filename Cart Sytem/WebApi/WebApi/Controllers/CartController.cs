baa190e8ff3ebb607b6ed33c22360762cbc0d4a5
Chimwemwe13 <91372786+Chimwemwe13@users.noreply.github.com>
2021-09-25 13:03:04 +0200

    Add files via upload

---
+++ Cart Sytem/WebApi/WebApi/Controllers/CartController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebApi.Models;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CartController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            {
+                string query = @"
+                    select ProductId, ProductName,PriceOfProduct from dbo.CartSystem";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("ItemsAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+
+        }
+        [HttpPost]
+        public JsonResult Post(CartSystem dep)
+        {
+            string query = @"
+                    insert into dbo.CartSystem
+                   (ProductName,PriceOfProduct)
+                    values 
+                    ('" + dep.ProductName + @"')
+                    ('" + dep.PriceOfProduct + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ItemsAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+
+
+
+
+        }
+        [HttpPut]
+        public JsonResult Put(CartSystem dep)
+        {
+            string query = @"
+                    update dbo.CartSystem set 
+                   ,ProductName = '" + dep.ProductName + @"'
+                   ,PriceOfProduct = '" + dep.PriceOfProduct + @"'
+                    where ProductId  = " + dep.ProductId + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ItemsAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.CartSystem
+                    where DepartmentId = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ItemsAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
